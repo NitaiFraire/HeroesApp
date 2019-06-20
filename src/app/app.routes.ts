6930fc0724335c1c55f1531246f30b395ac60d24
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-06-20 14:33:57 -0500

    busqueda por heroes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 
 const APP_ROUTES: Routes = [
@@ -11,7 +12,9 @@ const APP_ROUTES: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'heroes', component: HeroesComponent },
     { path: 'heroe/:id', component: HeroeComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    { path: 'buscar/:termino', component: BusquedaComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
